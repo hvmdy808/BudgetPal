2d57a956960871bfe8feb10b514c7e61754acf44
Mohamed Nabil Mohamed Abusarea <mnbil0680@gmail.com>
2025-05-12 04:08:34 +0300

    implement sample functions at JSONDealer

--- src/main/java/org/example/JSONDealer.java
+++ src/main/java/org/example/JSONDealer.java
@@ -1,4 +1,18 @@
 package org.example;
 
+import java.util.ArrayList;
+
 public class JSONDealer {
+    private JSONDealingStrategy strategy; // Using the correct interface name
+
+    public void setStrategy(JSONDealingStrategy newStrategy) {
+        this.strategy = newStrategy;
+    }
+
+    public void dealWithJSON(ArrayList<User> usersList) {
+        if (strategy == null) {
+            throw new IllegalStateException("No strategy has been set for the JSONDealer");
+        }
+        strategy.Deal(usersList); // Calling the lowercase method name
+    }
 }
